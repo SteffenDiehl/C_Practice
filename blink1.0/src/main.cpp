18a8f2308816f41cb1424fe7224c652cf9bb0347
SteffenDiehl <Steffen.Diehl@rdf.nuernberg.de>
2023-05-18 19:09:02 +0200

    web

--- blink1.0/src/main.cpp
+++ blink1.0/src/main.cpp
@@ -1,9 +1,111 @@
-#include <Arduino.h>
+#include <Arduino.h> 
+#include <RotaryEncoder.h> 
+#include <Wire.h> 
+#include <Adafruit_GFX.h> 
+#include <Adafruit_SSD1306.h>
+ 
 
-void setup() {
-  // put your setup code here, to run once:
-}
+#define PIN_IN1 36 
 
-void loop() {
-  // put your main code here, to run repeatedly:
+#define PIN_IN2 39 
+
+ 
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels 
+
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels 
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins) 
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, 4); 
+ 
+
+// Setup a RotaryEncoder with 4 steps per latch for the 2 signal input pins: 
+
+// RotaryEncoder encoder(PIN_IN1, PIN_IN2, RotaryEncoder::LatchMode::FOUR3); 
+
+// Setup a RotaryEncoder with 2 steps per latch for the 2 signal input pins: 
+
+RotaryEncoder encoder(PIN_IN1, PIN_IN2, RotaryEncoder::LatchMode::TWO03); 
+
+ 
+
+char *menu[] = {"Eier hart", "Eier weich", "Schweinsbraten", "Rueckentraining"}; 
+
+int anzahl = 4;
+
+void setup() 
+
+{ 
+Serial.begin(115200); 
+
+if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
+{ // Address 0x3D for 128x64 
+
+Serial.println(F("SSD1306 allocation failed")); 
+
+for(;;); 
+
+} 
+
+delay(2000); 
+
+display.clearDisplay(); 
+
+display.setTextSize(1); 
+
+display.setTextColor(WHITE); 
+
+display.setCursor(0, 10); 
+
+// Display static text 
+
+display.println("Hello, EAV2A."); 
+
+display.display();  
+
+while (! Serial); 
+
+Serial.println("Einfaches Beispie zum RE"); 
+
+} // setup() 
+
+ 
+
+// Read the current position of the encoder and print out when changed. 
+
+void loop() 
+
+{ 
+
+static int pos = 0; 
+
+int wahl = 0; 
+
+encoder.tick(); 
+
+int newPos = encoder.getPosition(); 
+
+if (pos != newPos) { 
+
+Serial.print("pos:"); 
+
+Serial.print(newPos); 
+
+Serial.print(" dir:"); 
+
+Serial.println((int)(encoder.getDirection())); 
+
+wahl = newPos % anzahl; 
+
+wahl = wahl < 0 ? -wahl : wahl; 
+
+Serial.println(menu[wahl]); 
+
+Serial.println(newPos); 
+
+pos = newPos; 
+
+} // if 
+
 }
---
+++ pointer.c
@@ -0,0 +1,14 @@
+void setup()
+{
+    int a = 5;
+    int b = 8;
+}
+void Swap(int *x, int *y)
+{
+    
+}
+void main()
+{
+    Swap(&a, &b);
+    return 0;
+} 
